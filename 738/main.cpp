1dfb5e3abe72ca8905c478caa7e2ea214355b447
lfnast <luyuritian@gmail.com>
2023-10-10 19:40:53 +0800

    20231010

--- 738/main.cpp
+++ 738/main.cpp
@@ -14,54 +14,60 @@ int monotoneIncreasingDigits(int n) {
     // divide the num to an array, if the num is inceased then return it
     // if not, find the first decreased point and return it like xx_9999999...
     vector<int> num;
-    int cnt = 0;
-    int cnt_2 = 0;
+    int tmp_n = n % 10;
+    int tmp_s = 0;
     int out = 0;
-    int n_copy = n;
-    bool flag = true;
-    while (n_copy > 0){
-        num.emplace_back(n_copy % 10);
-        n_copy /= 10;
+    num.emplace_back(tmp_n);
+    int sub = 0;
+    int n_cp = n;
+    int cnt = 0;
+    while (n_cp > 0){
         cnt++;
-    }
-    vector<int> num_cp = num;
-    sort(num.rbegin(), num.rend());
-    for (int i = 0; i < cnt; i++){
-        if (num[i] != num_cp[i]){
-            flag = false;
+        n_cp /= 10;
+        int tmp = n_cp % 10;
+        num.emplace_back(tmp);
+        if (tmp_n < tmp){
+            tmp_n = tmp - 1;
+            tmp_s = tmp - 1;
+            sub = cnt;
+            continue;
         }
+        tmp_n = tmp;
+
     }
-    if (flag){
-        return n;
+    num.pop_back(); // 0
+
+    for (auto m = 0; m < num.size(); m++){
+        cout << num[m] << '\t';
+    }
+    cout << endl;
+
+    for (int i = 0; i < sub; i++){
+        num[i] = 9;
     }
 
-    for (int j = cnt-1; j > 0; j--){
-        if (num_cp[j] > num_cp[j-1]){
-            cnt_2 = j;
+    if (sub){
+        num[sub] = tmp_s;
+    }
+
+    for (auto m = 0; m < num.size(); m++){
+        cout << num[m] << '\t';
+    }
+    cout << endl;
+
+    for (int j = cnt-1; j >= 0; j--){
+        out += num[j];
+        if (!j){
             break;
         }
-    }
-    cout << cnt << cnt_2 <<endl;
-    for (int k = cnt-1; k >= 0; k--){
-
         out *= 10;
-        if (k == cnt_2){
-            out += num_cp[k]-1;
-            continue;
-        }
-        if (k < cnt_2){
-            out += 9;
-            continue;
-        }
-        out += num_cp[k];
-        
     }
     return out;
 }
 
 
 int main(int argc, char **argv){
-    int n = 1333332;
+    int n = 1234;
 
     cout << n << endl << monotoneIncreasingDigits(n) << endl;
     exit(0);
